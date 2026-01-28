187214c21f50404932835e44f6cc90d5b3d3b66a
Tamás Gyetvai <34650749+gyetvaitamas@users.noreply.github.com>
2026-01-28 22:20:04 +0100

    created player and movement

---
+++ scripts/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+class_name Player
+
+const SPEED = 100.0
+
+var last_direction: Vector2 = Vector2.RIGHT
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(_delta: float) -> void:
+	process_movement()
+	process_animation()
+	move_and_slide()
+
+func process_movement() -> void:
+		# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_vector("left", "right", "up", "down")
+	
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		last_direction = direction
+	else:
+		velocity = Vector2.ZERO
+
+func process_animation() -> void:
+	if velocity != Vector2.ZERO:
+		play_animation("run", last_direction)
+	else:
+		play_animation("idle", last_direction)
+
+func play_animation(prefix: String, direction: Vector2) -> void:
+	if direction.x != 0:
+		animated_sprite_2d.flip_h = direction.x < 0
+		animated_sprite_2d.play(prefix + "_right")
+	elif direction.y < 0:
+		animated_sprite_2d.play(prefix + "_up")
+	elif direction.y > 0:
+		animated_sprite_2d.play(prefix + "_down")
